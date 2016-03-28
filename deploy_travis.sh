19be0f0cb847ce9272227aedae993eda4b7b3545
if1live <libsora25@gmail.com>
2016-03-28 10:42:24 +0900

    travis 빌드 시도

---
+++ deploy_travis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git clone --depth 1 --quiet https://${GH_TOKEN}@github.com/5minlab/vr.5minlab.com.git --branch gh-pages
+
+rm -rf gh-pages/*
+ls -al gh-pages
+
+./deploy_local.sh
+cp -r output/* gh-pages
+
+cd gh-pages
+git add -f .
+diff_line=$(git status -s|wc -l)
+if [[ $diff_line == "0" ]]; then
+	echo "there is no changes, do not commit"
+else
+	git commit -a -m "add new site content"
+	git push https://${GH_TOKEN}@github.com/5minlab/vr.5minlab.com.git gh-pages --quiet
+fi
